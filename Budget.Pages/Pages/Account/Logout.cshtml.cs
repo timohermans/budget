938cfefe448081e4711176f0e9c6c9fa91e64e2b
Timo Hermans <tmhermans@gmail.com>
2023-12-09 09:36:29 +0100

    Add logout functionality

---
+++ Budget.Pages/Pages/Account/Logout.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Budget.Pages.Pages.Account;
+
+public class Logout : PageModel
+{
+    public async Task<IActionResult> OnPost()
+    {
+        await HttpContext.SignOutAsync();
+        return RedirectToPage("/Account/Login");
+    }
+}
