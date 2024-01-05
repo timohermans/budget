311ad4c2c42417dbd9d27672578571ccfd6f5e1c
Timo Hermans <tmhermans@gmail.com>
2024-01-05 12:53:10 +0100

    Make it possible to unmark a cashback transaction

--- Budget.IntegrationTests/Tests/Core/UseCases/TransactionMarkAsCashbackUseCaseTests.cs
+++ Budget.IntegrationTests/Tests/Core/UseCases/TransactionMarkAsCashbackUseCaseTests.cs
@@ -25,13 +25,53 @@ public class TransactionMarkAsCashbackUseCaseTests(TestFixture fixture, ITestOut
 
         transactionToMark.PartitionKey.Should().Be("2024-1");
 
-        var actual = new TransactionMarkAsCashbackUseCase(db, new XunitLogger<TransactionMarkAsCashbackUseCase>(output))
+        var result = new TransactionMarkAsCashbackUseCase(db, new XunitLogger<TransactionMarkAsCashbackUseCase>(output))
             .Handle(
                 new TransactionMarkAsCashbackUseCase.Request(transactionToMark.RowKey,
                     transactionToMark.PartitionKey,
                     new DateTime(2024, 1, 2)));
 
+        result.Should().BeAssignableTo<SuccessResult<Transaction>>();
+        result.Data.RowKey.Should().Be(transactionToMark.RowKey);
+        var actual = db.Query<Transaction>(q => q.RowKey == transactionToMark.RowKey).First();
+        actual.PartitionKey.Should().Be("2024-1");
+        actual.CashbackForDate.Should().Be(new DateTime(2024, 1, 2, 0, 0, 0, DateTimeKind.Utc));
+    }
+
+    [Fact]
+    public async Task Unmark_cashback_transaction()
+    {
+        var db = await fixture.CreateTableClientAsync();
+        var date = new DateTime(2024, 1, 5, 0, 0, 0, DateTimeKind.Utc);
+        var transactionToUnmark = new Transaction
+        {
+            Currency = "EUR",
+            Iban = "NL22RABO0101044666",
+            DateTransaction = date,
+            FollowNumber = 1121,
+            PartitionKey = Transaction.CreatePartitionKey(date),
+            RowKey = "NL22RABO0101044666-1121",
+            Amount = 151,
+            Description = "Cambrium Tweak BV",
+            IbanOtherParty = "NL33INGB0000000000",
+            AuthorizationCode = "CODE66",
+            BalanceAfterTransaction = 1000,
+            CashbackForDate = date,
+            NameOtherParty = "Cambrium BV",
+        };
+        await db.AddEntityAsync(transactionToUnmark);
+
+        var actual = new TransactionMarkAsCashbackUseCase(db, new XunitLogger<TransactionMarkAsCashbackUseCase>(output))
+            .Handle(
+                new TransactionMarkAsCashbackUseCase.Request(transactionToUnmark.RowKey,
+                    transactionToUnmark.PartitionKey,
+                    null));
+
         actual.Should().BeAssignableTo<SuccessResult<Transaction>>();
-        actual.Data.PartitionKey.Should().Be("2024-1");
+
+        var transaction = db.Query<Transaction>(q => q.RowKey == transactionToUnmark.RowKey).First();
+
+        transaction.Should().NotBeNull();
+        transaction.CashbackForDate.Should().BeNull();
     }
 }
