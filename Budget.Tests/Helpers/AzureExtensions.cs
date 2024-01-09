3dd43f0caa48742358156a676818b6ae6adfd1ca
Timo Hermans <tmhermans@gmail.com>
2024-01-09 16:55:12 +0100

    Add spaarmeter unit tests

---
+++ Budget.Tests/Helpers/AzureExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Azure;
+using FakeItEasy;
+
+namespace Budget.Tests.Helpers;
+
+public static class AzureExtensions
+{
+    public static Pageable<T> ToPageable<T>(this IEnumerable<T> list) =>
+        Pageable<T>.FromPages([
+            Page<T>.FromValues(list.ToList().AsReadOnly(), null, A.Fake<Response>())
+        ]);
+}
