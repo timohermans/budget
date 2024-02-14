225dbfaa4975b1f29cfd40958fedd8b7a9ce9477
Timo Hermans <tmhermans@gmail.com>
2024-02-14 11:49:15 +0100

    Log when someone is trying to log in again

--- Budget.App/Apis/LoginLogout/LoginController.cs
+++ Budget.App/Apis/LoginLogout/LoginController.cs
@@ -4,8 +4,9 @@ namespace Budget.App.Apis.LoginLogout;
 
 public static class LoginController
 {
-    public static async Task GetLogin(HttpContext context, ILogger logger)
+    public static async Task GetLogin(HttpContext context, ILoggerFactory loggerFactory)
     {
+        var logger = loggerFactory.CreateLogger(nameof(LoginController));
         logger.LogInformation("Someone is trying to log in.");
         
         await context.ChallengeAsync(new AuthenticationProperties
