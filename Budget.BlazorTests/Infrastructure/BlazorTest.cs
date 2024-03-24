7a0ae090ac84b82c85a3022da901be79cb7bd059
Timo Hermans <tmhermans@gmail.com>
2024-03-24 07:40:25 +0100

    Try fix e2e tests on ci/cd

--- Budget.BlazorTests/Infrastructure/BlazorTest.cs
+++ Budget.BlazorTests/Infrastructure/BlazorTest.cs
@@ -30,7 +30,6 @@ public class BlazorTest : PageTest
     [SetUp]
     public async Task BeforeEach()
     {
-        Page.SetDefaultTimeout(5000);
         await DatabaseHelper.ResetDataAsync();
     }
 
