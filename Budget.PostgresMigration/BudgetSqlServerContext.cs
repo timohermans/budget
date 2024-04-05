fd2de76d404fe7d8f293313119ff13077295cbef
Timo Hermans <tmhermans@gmail.com>
2024-04-05 17:30:49 +0200

    Add project to migrate data from postgres to sql server

---
+++ Budget.PostgresMigration/BudgetSqlServerContext.cs
@@ -0,0 +1,19 @@
+﻿using Budget.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Budget.PostgresMigration;
+
+public class BudgetSqlServerContext : DbContext
+{
+    public DbSet<Transaction> Transactions => Set<Transaction>();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var config = new ConfigurationBuilder()
+                          .AddJsonFile("appsettings.json")
+                          .Build();
+
+        optionsBuilder.UseSqlServer(config.GetConnectionString("SqlServer"));
+    }
+}
---
+++ Budget.PostgresMigration/Program.cs
@@ -0,0 +1,34 @@
+﻿using Budget.Core.DataAccess;
+using Budget.PostgresMigration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+var config = new ConfigurationBuilder()
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+
+var postgresOptions = new DbContextOptionsBuilder<BudgetContext>()
+                            .UseNpgsql(config.GetConnectionString("Postgres"))
+                            .Options;
+var dbPostgres = new BudgetContext(postgresOptions);
+
+Console.WriteLine($"Fetching psql transactions...");
+
+var transactions = await dbPostgres.Transactions.ToListAsync();
+
+Console.WriteLine($"Fetched {transactions.Count} transactions");
+
+var dbSqlServer = new BudgetSqlServerContext();
+
+transactions.ForEach(t =>
+{
+    t.Id = 0;
+});
+
+await dbSqlServer.Transactions.AddRangeAsync(transactions);
+
+await dbSqlServer.SaveChangesAsync();
+
+Console.WriteLine("Inserted all transactions");
+
+Console.WriteLine("Done migrating!");
