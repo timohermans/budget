5f0f7c3d3a6fcf6789a38903c213416c1ececa33
Timo Hermans <tmhermans@gmail.com>
2024-10-20 18:08:05 +0000

    Add logging to deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 current_date=$(date '+%Y-%m-%d %H:%M:%S')
+app_name="Budget"
 
 last_tag_file=".last_deployed_tag"
 
 log_message() {
-  echo "[${current_date}] $1"
+  local level=$1
+  local message=$2
+  logger -s -t "${app_name}" -p "user.${level,,}" "${message}"
 }
 
 get_latest_tag() {
@@ -15,21 +18,21 @@ get_latest_tag() {
 
 # Check if we're in a git repository
 if ! git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
-    log_message "Error: Not a git repository"
+    log_message "ERROR" "Error: Not a git repository"
     exit 1
 fi
 
 latest_tag=$(get_latest_tag)
 
 if [ -z "$latest_tag" ]; then
-    log_message "No tags found in the repository"
+    log_message "ERROR" "No tags found in the repository"
     exit 0
 fi
 
 # Check if the last known tag file exists
 if [ ! -f "$last_tag_file" ]; then
     echo "-1" > $last_tag_file
-    log_message "Initialized with tag: $latest_tag"
+    log_message "INFO" "Initialized with tag: $latest_tag"
 fi
 
 # Read the last known tag
@@ -37,19 +40,19 @@ last_known_tag=$(cat "$last_tag_file")
 
 # Compare tags
 if [ "$latest_tag" != "$last_known_tag" ]; then
-    log_message "New tag detected: $latest_tag (previous: $last_known_tag)"
-    log_message "Deploying..."
+    log_message "INFO" "New tag detected: $latest_tag (previous: $last_known_tag)"
+    log_message "INFO" "Deploying..."
     docker compose pull
     docker compose up -d
     source .env
     docker run --network postgres --env "ConnectionStrings__BudgetContext=$ConnectionStrings__BudgetContext" budget-migrations
-    log_message "Deployed!"
+    log_message "INFO" "Deployed!"
     echo "$latest_tag" > "$last_tag_file"
-    log_message "Saved latest tag"
+    log_message "INFO" "Saved latest tag"
     exit 0
 else
-    log_message "No new tags found. Latest tag: $latest_tag"
-    log_message "No need to deploy anything..."
+    log_message "INFO" "No new tags found. Latest tag: $latest_tag"
+    log_message "INFO" "No need to deploy anything..."
     exit 0
 fi
 
