53f5d434caa5e8d67063d42eb08709accb65de33
Timo Hermans <tmhermans@gmail.com>
2025-08-17 15:16:54 +0200

    feat: Make it way easier to develop Budget locally

---
+++ Budget.Aspire/AppHost.cs
@@ -0,0 +1,35 @@
+using Projects;
+
+var builder = DistributedApplication.CreateBuilder(args);
+
+var rabbit = builder.AddRabbitMQ("rabbit");
+
+var postgres = builder.AddPostgres("budget-db")
+    .WithPgAdmin()
+    .WithHostPort(5050)
+    .WithDataVolume(isReadOnly: false)
+    .AddDatabase("budgetdb");
+
+var migrations = builder.AddProject<Budget_MigrationsRunner>("budget-migrations")
+    .WithReference(postgres)
+    .WaitFor(postgres);
+
+var budgetApi = builder.AddProject<Budget_Api>("budget-api")
+    .WithReference(rabbit)
+    .WithReference(postgres)
+    .WaitFor(postgres)
+    .WaitFor(migrations); // no need to wait for rabbitmq, as it slows down the startup
+
+var worker = builder.AddProject<Budget_Worker>("budget-worker")
+    .WithReference(rabbit)
+    .WithReference(postgres)    
+    .WaitFor(postgres)
+    .WaitFor(rabbit)
+    .WaitFor(migrations);
+
+builder.AddProject<Budget_App>("budget-app")
+    .WithReference(budgetApi)
+    .WithEnvironment("BudgetApi__BaseUrl", budgetApi.GetEndpoint("https"))
+    .WaitFor(budgetApi);
+
+builder.Build().Run();
