7de333ba3bfa7b7bafc156fe9fa53b7a5aeb55eb
Timo Hermans <tmhermans@gmail.com>
2025-08-31 07:41:50 +0200

    feat: add BatchId, Code, and Reference columns to Transactions table (migrations)

---
+++ src/Budget.Infrastructure/Migrations/20250830184756_AlterTransactionAddCodeReferenceBatchId.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Budget.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlterTransactionAddCodeReferenceBatchId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BatchId",
+                table: "Transactions",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Code",
+                table: "Transactions",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Reference",
+                table: "Transactions",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BatchId",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "Code",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "Reference",
+                table: "Transactions");
+        }
+    }
+}
