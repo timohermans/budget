f5c3d4e2d91fea885b85d279aba71d862832b9df
Timo Hermans <tmhermans@gmail.com>
2026-02-04 07:14:07 +0100

    fix: update appconfig and routes

--- src/budget-angular/src/app/app.routes.ts
+++ src/budget-angular/src/app/app.routes.ts
@@ -1,11 +1,21 @@
 import { Routes } from '@angular/router';
 import { Budget } from '../budget/budget.component';
+import { FakeLoginComponent } from '../auth/fake-login.component';
 import { authGuard } from '../auth/auth.guard';
 
 export const routes: Routes = [
   {
+    path: '',
+    redirectTo: '/budget',
+    pathMatch: 'full'
+  },
+  {
     path: 'budget',
     component: Budget,
     canActivate: [authGuard]
+  },
+  {
+    path: 'fake-login',
+    component: FakeLoginComponent
   }
 ];
