766982b431e2b09bb45ad332b4df2a50b3b90a8a
Timo Hermans <tmhermans@gmail.com>
2026-03-28 09:09:12 +0100

    chore: Style the header to be mobile responsive

--- src/budget-angular/src/budget/budget.component.ts
+++ src/budget-angular/src/budget/budget.component.ts
@@ -12,14 +12,14 @@ import { IbanBalancesComponent } from "./iban-balances.component";
   template: `
     <app-navbar></app-navbar>
     <div class="max-w-3xl mx-auto p-4 flex flex-col gap-4">
-      <div class="flex justify-between">
-        <div class="flex flex-col gap-2">
+      <div class="flex flex-col sm:flex-row gap-3 justify-between">
+        <div class="order-3 sm:order-1 flex flex-col gap-2">
           <h2 class="font-title text-2xl md:text-3xl lg:text-4xl font-bold">Overzicht</h2>
         </div>
 
-        <app-transactions-upload></app-transactions-upload>
+        <app-transactions-upload class="order-2 hidden sm:block"></app-transactions-upload>
 
-        <app-date-navigation />
+        <app-date-navigation class="order-1 sm:order-3 self-center" />
       </div>
 
       @if (transactionService.isLoading()) {
